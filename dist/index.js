55bd4e5ab5e0eccb4b79574f303dc8e6052bf7c2
h <72895702+KSPAtlas@users.noreply.github.com>
2022-03-17 17:19:30 +0000

    Delete dist directory

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.whatWeWant = exports.default = void 0;
-var whatWeWant = '♥';
-exports.default = whatWeWant;
-exports.whatWeWant = whatWeWant;
